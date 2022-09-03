ef2e87d1c32b4d00a6fe87be4a5789250be52a06
ryoichi-k <kazyuapple99@gmail.com>
2022-09-03 11:02:28 +0900

    try shift_only

--- Python_main_sample.py
+++ Python_main_sample.py
@@ -4,9 +4,13 @@ import sys
 
 
 def main():
+    # 数字を受け取る
     N = int(input())
+
+    #　配列として受け取る（スペースで区切って入力すること）
     A = list(map(int, input().split()))
     print(N,A)
+
 
 
 if __name__ == "__main__":
--- Shiftonly.py
+++ Shiftonly.py
@@ -9,6 +9,25 @@ def main():
     A = list(map(int, input().split()))
     print(N,A)
 
+    # 操作回数
+    count = 0
+
+    while True:
+        can_do = True
+        for i in range(N):
+            if A[i] % 2 == 1:
+                can_do = False
+
+        if not can_do:
+            break
+
+        for i in range(N):
+            A[i] //= 2
+
+        count += 1
+
+    print(count)
+
 
 if __name__ == "__main__":
 
