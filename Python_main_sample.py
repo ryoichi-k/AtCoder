bc6c3ee93e9ede6c624c6bfb3855bec7e681ca7e
ryoichi-k <kazyuapple99@gmail.com>
2022-09-03 10:57:29 +0900

    python default file

---
+++ Python_main_sample.py
@@ -0,0 +1,17 @@
+import sys
+
+
+
+
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    print(N,A)
+
+
+if __name__ == "__main__":
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
