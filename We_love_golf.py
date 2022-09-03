925ccc6117017e4c855938596d23c109d11e9dd4
ryoichi-k <kazyuapple99@gmail.com>
2022-09-03 13:23:49 +0900

    done

--- We_love_golf.py
+++ We_love_golf.py
@@ -6,11 +6,29 @@ import sys
 def main():
     print("当ファイルの実行に成功")
     # 数字を受け取る
-    N = int(input())
+    K = int(input())
 
     #　配列として受け取る（スペースで区切って入力すること）
-    A = list(map(int, input().split()))
-    print(N,A)
+    A, B = map(int, input().split())
+    print(A,B)
+
+    exist = False
+
+    for i in range(A, B + 1):
+        if i % K == 0:
+            print("in")
+            exist == True
+
+    # print("OK" if exist == True else "NG")
+    print(exist)
+
+    if exist:
+        print("ok")
+    else:
+        print("ng")
+
+
+
 
 
 
